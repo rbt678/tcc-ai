d6fa3f788847f18c9e2487a399ab2ad3fa510997
Roberto Javier Lagger <javierlaggerr@gmail.com>
2023-09-01 03:05:38 -0300

    c

--- Assistant.py
+++ Assistant.py
@@ -1,95 +1,69 @@
 import openai
+import os
 from re import sub
 from docx import Document
 from spacy import load
 from chromadb import PersistentClient
 from sentence_transformers import SentenceTransformer
+from chromadb.config import Settings
 
 class ChromaDBManager:
-    """
-    Gerencia operações relacionadas ao ChromaDB, como consulta e inserção.
-    """
 
     def __init__(self, path:str, collection_name:str, embedding_function="all-MiniLM-L6-v2"):
-        """
-        Inicializa a classe ChromaDBManager para gerenciar uma coleção no ChromaDB.
-        
-        :param path: Caminho para o banco de dados.
-        :param collection_name: Nome da coleção no banco de dados.
-        :param embedding_function: Nome da função de embedding. Padrão é "all-MiniLM-L6-v2".
-        """
-        # Se a função de embedding for o padrão, inicialize o modelo
         if embedding_function == "all-MiniLM-L6-v2":
             self.model = SentenceTransformer("all-MiniLM-L6-v2")
             ef = self.__funcao_embbeding
         else: 
             return False
 
-        # Estabelece conexão com o banco de dados e obtém ou cria a coleção
-        self.client = PersistentClient(path=path)
+        self.client = PersistentClient(path=path, settings=Settings(allow_reset=True))
         self.collection = self.client.get_or_create_collection(name=collection_name, embedding_function=ef)
 
     def __funcao_embbeding(self, textos:list[str]) -> list[str]:
-        """
-        Calcula os embeddings para uma lista de textos usando SentenceTransformer.
-        
-        :param textos: Lista de textos.
-        :return: Lista de embeddings.
-        """
         return self.model.encode(sentences=textos).tolist()
+    
+    def __preprocessar_docx(self, doc):
+        nlp = load("en_core_web_sm")
+        text = '\n'.join(parag.text for parag in doc.paragraphs)
+        clean_text = sub(r'\W+', ' ', text).lower()
 
+        return nlp(clean_text)
+    
     def get_max_id(self) -> int:
-        """
-        Obtém o ID máximo da coleção.
-        
-        :return: O próximo ID disponível na coleção.
-        """
         ids_str = self.collection.get()["ids"]
+        
         return max([int(num) for num in ids_str]) + 1 if ids_str else 0
 
-    def add_na_collection(self, list_ids, list_texts):
-        """
-        Adiciona documentos à coleção.
+    def ler_todos_docxs(self, path:str) -> list[str]:
+        sentences = []
+
+        for filename in os.listdir(path):
+            if filename.endswith(".docx"):
+                doc = Document(os.path.join(path, filename))
+                preprocess = self.__preprocessar_docx(doc)
+                sentences.extend([sent.text for sent in preprocess.sents])
+
+        if sentences==[]:
+            print("Não há arquivos .docx na pasta")
+            return False
         
-        :param list_ids: Lista de IDs.
-        :param list_texts: Lista de textos/documentos.
-        """
-        self.collection.add(ids=list_ids, documents=list_texts)
+        sentences_ids = [str(i) for i in range(len(sentences))]
+        self.collection.add(documents=sentences, ids=sentences_ids)
 
     def query(self, query_texts:str, n_results:int = 5) -> dict:
-        """
-        Consulta a coleção pelo texto da consulta.
-        
-        :param query_texts: Texto da consulta.
-        :param n_results: Número de resultados desejados. Padrão é 5.
-        :return: Resultados da consulta.
-        """
         return self.collection.query(query_texts=query_texts, n_results=n_results)
 
 
 class Assistant:
-    """
-    Classe auxiliar para interagir com o modelo GPT-3 e ChromaDB.
-    """
-
-    def __init__(self, db_manager: ChromaDBManager, api_key_path:str):
-        """
-        Inicializa a classe Assistant.
-        
-        :param db_manager: Instância da classe ChromaDBManager.
-        :param api_key_path: Caminho para a chave da API OpenAI.
-        """
-        self.db_manager = db_manager
-        self.openai_api_key = self._ler_arquivo(api_key_path)
+    def __init__(self, api_key_path:str = "credentials/apiKey", pasta_database:str = "dataBase", colecao:str = "colecao1"):
+        self.pasta_database = pasta_database
+        self.colecao = colecao
+        self.db_manager = ChromaDBManager(pasta_database, colecao)
+        self.openai_api_key = self.__ler_arquivo(api_key_path)
+        self.historico_atual = []
 
     @staticmethod
-    def _ler_arquivo(caminho:str) -> str:
-        """
-        Lê o conteúdo de um arquivo.
-        
-        :param caminho: Caminho do arquivo.
-        :return: Conteúdo do arquivo.
-        """
+    def __ler_arquivo(caminho:str) -> str:
         try:
             with open(caminho, "r") as file:
                 return file.read().strip()
@@ -97,44 +71,23 @@ class Assistant:
             print(f"O arquivo '{caminho}' não foi encontrado.")
             return ""
 
-    @staticmethod
-    def _extrair_texto_docx(caminho:str) -> str:
-        """
-        Extrai texto de um arquivo DOCX.
-        
-        :param caminho: Caminho do arquivo DOCX.
-        :return: Texto extraído.
-        """
-        doc = Document(caminho)
-        return '\n'.join(parag.text for parag in doc.paragraphs)
-
-    def preprocess(self, texto:str) -> list[str]:
-        """
-        Pré-processa o texto, removendo caracteres especiais e dividindo em sentenças.
-
-        :param texto: Texto a ser pré-processado.
-        :return: Lista de sentenças.
-        """
-        clean_text = sub(r'\W+', ' ', texto).lower()
-        nlp = load('en_core_web_sm')
-        doc = nlp(clean_text)
-        return [p.text for p in doc.sents]
-
     def enviar_gpt(self, system_role:str, database:str, quest:str, collection_response) -> dict:
-        """
-        Interage com o modelo GPT-3 usando a chave da API fornecida.
-
-        :param system_role: Mensagem inicial para o modelo.
-        :param database: Base de dados para a consulta.
-        :param quest: Pergunta do usuário.
-        :return: Resposta do modelo GPT-3.
-        """
-
         openai.api_key = self.openai_api_key
+
+        historico = self.historico_atual[-20:]
+        historico_string = ""
+        for dict in historico:
+            if dict["role"] == "user":
+                historico_string += f"User: {dict['content']}\n"
+            if dict["role"] == "assistant":
+                historico_string += f"Assistant: {dict['content']}\n"
+
         prompt = [
             {"role": "system", "content": system_role},
-            {"role": "user", "content": f"'database':\n###\n{database}\n###\n\nPergunta: {quest}\n"}
+            {"role": "user", "content": f"'database':\n###\n{database}\n###\n\n 'historico':\n###\n{historico_string}\n###\n\n Pergunta do usuário: {quest}\n"}
         ]
+
+        
         
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -148,8 +101,7 @@ class Assistant:
         
         return {'response': response, 'prompt': prompt, 'collection_response': collection_response}
     
-    def gpt_tradutor(self, pergunta:str):
-
+    def gpt_tradutor_en(self, pergunta:str):
         openai.api_key = self.openai_api_key
         prompt=[{"role": "system","content": "Por favor traduza para o inglês tudo que o usuário enviar. E se o usuário enviar algo em inglês repita exatamente o que ele disse."},
                 {"role": "user","content": "I like guarana, and you?"},
@@ -171,48 +123,33 @@ class Assistant:
         return {'response': response, 'prompt': prompt}
 
     def query(self, pergunta:str) -> dict:
-        """
-        Realiza uma consulta na base de dados e depois consulta o modelo GPT-3.
-
-        :param pergunta: Pergunta do usuário.
-        :return: Resposta do modelo GPT-3.
-        """
-
-        pergunta_eng = self.gpt_tradutor(pergunta=pergunta)["response"]["choices"][0]["message"]["content"]
+        pergunta_eng = self.gpt_tradutor_en(pergunta=pergunta)["response"]["choices"][0]["message"]["content"]
         collection_response = self.db_manager.query(query_texts=pergunta_eng, n_results=5)
         collection_database = collection_response["documents"][0]
         system_role = (
-            "You are an intelligent, polite, and dedicated assistant from Hakunamatata company. "
-            "If you can't find the answer from the given 'database', "
-            "please reply with: 'Infelizmente essa informação não está na minha base de dados.'. "
-            "You have to respond using a maximum of 150 tokens. "
-            "Now, based on the following database..."
+            "Você é um assistente inteligente, educado e dedicado da empresa Hakunamatata, que responde perguntas do usuário. "
+            "Você possui 'dataBase' para buscar informações e 'historico' para lembrar a conversa com o usuário. "
+            "Se você não souber como responder o usuário, responda com: 'Infelizmente essa informação não está na minha base de dados.' "
+            "Você deve responder usando um máximo de 150 tokens. "
+            "Agora, com base nos seguintes dados, responda a pergunta do usuário: "
         )
 
         return self.enviar_gpt(system_role=system_role, database=collection_database, quest=pergunta, collection_response=collection_response)
-
-    def add_document_into_collection(self, caminho_docx):
-        """
-        Adiciona um documento no formato DOCX à coleção.
-
-        :param caminho_docx: Caminho para o arquivo DOCX a ser adicionado.
-        """
-        clean_text = self.preprocess(self._extrair_texto_docx(caminho_docx))
-        id_atual = self.db_manager.get_max_id() + 1
-        list_ids = list(range(id_atual, id_atual+len(clean_text)))
-        list_ids_str = [str(num) for num in list_ids]
-        self.db_manager.add_na_collection(list_ids=list_ids_str, list_texts=clean_text)
+    
+    def atualizar_documentos(self):
+        self.db_manager.client.reset()
+        self.db_manager = ChromaDBManager(self.pasta_database, self.colecao)
+        self.db_manager.ler_todos_docxs("documentos")
 
 
-def processar_pergunta(pergunta):
-    db_manager = ChromaDBManager("baseDados", "colecao5")
-    assistant = Assistant(db_manager=db_manager, api_key_path="credentials/apiKey")
-    query_return = assistant.query(pergunta)
+    def salvar_historico_atual(self, historico):
+        self.historico_atual.extend(historico)
+        
 
-    return query_return
 
 
 if __name__=="__main__":
-    db_manager = ChromaDBManager("baseDados", "colecao5")
-    assistant = Assistant(db_manager, "credentials/apiKey")
-    #assistant.add_document_into_collection("Documentacao_ChromaDB.docx")
+    assistente = Assistant()
+    #print(assistente.atualizar_documentos())
+    #print(assistente.db_manager.client.list_collections())
+    print(assistente.query("O que é ChromaDB?"))
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import Assistant
 
 app = Flask(__name__)
+assistant = Assistant.Assistant()
 
 @app.route('/')
 def index():
@@ -12,12 +13,19 @@ def index():
 def ask():
     data = request.json
     user_message = data["message"]
-    process_return = Assistant.processar_pergunta(user_message)
+    process_return = assistant.query(pergunta=user_message)
+    respostaGPT = process_return["response"]["choices"][0]["message"]["content"]
+    promptEnviado = str(process_return["prompt"])
+    respostaDaColecao = str(process_return['collection_response'])
+
+    dict_historico = [
+        {"role": "user","content": user_message},
+        {"role": "assistant","content": respostaGPT}
+    ]
+    assistant.salvar_historico_atual(dict_historico)
 
 
-    return jsonify({"response": process_return["response"]["choices"][0]["message"]["content"], 
-                    "prompt": process_return["prompt"][1]["content"], 
-                    "collection_response": str(process_return['collection_response'])})
+    return jsonify({"response": respostaGPT, "prompt": promptEnviado, "collection_response": respostaDaColecao})
 
 if __name__ == '__main__':
     app.run(debug=True)
